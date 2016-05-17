08732b98071026bc33ce3c560df3d1d761070ade
Ezandora <ezandora@gmail.com>
2016-05-17 10:43:09 -0500

    1.4
    The Source initial path support. Compatible with dataspider.

--- relay/relay_Guide.js
+++ relay/relay_Guide.js
@@ -1,5 +1,242 @@
 //This script is in the public domain.
 
+
+//Min, max inclusive.
+function randomi(min, max)
+{
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Matrix:
+var __matrix_spinners = [];
+var __matrix_spinner_counter = 0;
+var __matrix_animation_interval = 16;
+var __matrix_glyph_size = 16;
+var __matrix_animating = false;
+var __matrix_timer_id = 1;
+var __matrix_last_event_time;
+
+function matrixDrawGlyph(context, glyphs, glyph_index, x_index, y_index, alpha, colour)
+{
+	if (y_index < 0 || x_index < 0)
+		return;
+	var gx = glyph_index % 10;
+	var gy = Math.floor(glyph_index / 10);
+	
+	context.globalCompositeOperation = "source-over";
+	if (false)
+	{
+		context.globalAlpha = 1.0;
+		context.fillStyle = "rgb(0,0,0)";
+		context.fillRect(x_index * __matrix_glyph_size, y_index * __matrix_glyph_size, __matrix_glyph_size, __matrix_glyph_size);
+	}
+	//context.globalCompositeOperation = "lighter";
+	context.globalAlpha = alpha;
+	context.drawImage(glyphs, gx * __matrix_glyph_size, gy * __matrix_glyph_size, __matrix_glyph_size, __matrix_glyph_size, x_index * __matrix_glyph_size, y_index * __matrix_glyph_size, __matrix_glyph_size, __matrix_glyph_size);
+	//context.globalCompositeOperation = "source-over";
+	
+	if (colour != "")
+	{
+		context.globalCompositeOperation = "multiply";
+		context.fillStyle = colour; //"rgb(0,127,255)";
+		context.fillRect(x_index * __matrix_glyph_size, y_index * __matrix_glyph_size, __matrix_glyph_size, __matrix_glyph_size);
+		context.globalCompositeOperation = "source-over";
+	}
+}
+
+
+function matrixTick(id)
+{
+    if (id < __matrix_timer_id) //out of line
+        return;
+	var context_width = window.innerWidth; //document.body.clientWidth;
+	var context_height = window.innerHeight; //document.body.clientHeight;
+	var canvas = document.getElementById("matrix_canvas");
+    if (canvas == null)
+        return;
+	var canvas_holder = document.getElementById("matrix_canvas_holder");
+    if (canvas_holder == null)
+        return;
+	var needs_resize = false;
+	if (canvas.width != context_width)
+		needs_resize = true;
+	if (canvas.height != context_height)
+		needs_resize = true;
+	if (needs_resize)
+	{
+		var stored_data = undefined;
+		try
+		{
+			//This is an invalid operation on file:// in chrome, so catch and fallback:
+			stored_data = canvas.toDataURL();
+			var previous_image = new Image;
+			//Once the image has been parsed, resize and update:
+			previous_image.onload = function()
+			{
+				canvas.width = context_width;
+				canvas.height = context_height;
+				canvas.getContext("2d").drawImage(previous_image, 0, 0);
+				matrixTick(id);
+			}
+			previous_image.src = stored_data;
+			return;
+		}
+		catch (exception)
+		{
+			canvas.width = context_width;
+			canvas.height = context_height;
+		}
+	}
+	
+	var context = canvas.getContext("2d");
+	
+	var glyphs = document.getElementById("matrix_glyphs");
+	if (glyphs == null)
+        return;
+	
+	var maximum_glyphs_x = Math.ceil(context_width / __matrix_glyph_size);
+	var maximum_glyphs_y = Math.ceil(context_height / __matrix_glyph_size);
+	
+	
+	//Fade out:
+	if (true)
+	{
+		//Subtraction method:
+		context.globalCompositeOperation = "difference";
+		context.fillStyle = "rgb(2, 2, 2)";
+		context.globalAlpha = 1.0;
+		context.fillRect(0, 0, context_width, context_height);
+		context.globalCompositeOperation = "source-over";
+	}
+	else
+	{
+		//Alpha method:
+		context.fillStyle = "rgb(0,0,0)";
+		context.globalAlpha = 1.0 / __matrix_glyph_size; //1.0 / (60.0);
+		context.fillRect(0, 0, context_width, context_height);
+	}
+	
+	if (false)
+	{
+		//Fill the screen:
+		for (var y = 0; y < maximum_glyphs_y; y++)
+		{
+			for (var x = 0; x < maximum_glyphs_x; x++)
+			{
+				var glyph_index = randomi(0, 102 - 1);
+				var alpha = Math.random();
+				matrixDrawGlyph(context, glyphs, glyph_index, x, y, alpha, "");
+			}
+		}
+	}
+	if (false)
+	{
+		//Random glyphs:
+		for (var i = 0; i < 128; i++)
+		{
+			var x = randomi(0, maximum_glyphs_x);
+			var y = randomi(0, maximum_glyphs_y);
+			var glyph_index = randomi(0, 102 - 1);
+			var alpha = Math.random();
+			matrixDrawGlyph(context, glyphs, glyph_index, x, y, alpha, "");
+		}
+	}
+	
+	//Create spinners:
+	var spinner_density = (context_width * context_height) / (__matrix_glyph_size * 256);
+	while (__matrix_spinners.length < spinner_density)
+	{
+		var spinner = new Object();
+		
+		spinner.x_index = randomi(0, maximum_glyphs_x);
+		spinner.y_index = randomi(-16, maximum_glyphs_y);
+		spinner.alpha = Math.random(); //Math.random() * 0.25 + 0.75;
+		spinner.counter = randomi(0, 11);
+		spinner.glyph_index = randomi(0, 102 - 1);
+		spinner.interval = randomi(1, 15);
+		
+		__matrix_spinners.push(spinner);
+	}
+	//Draw spinners:
+	var spinners_next = [];
+	for (var i = 0; i < __matrix_spinners.length; i++)
+	{
+		var spinner = __matrix_spinners[i];
+		var should_delete = false;
+		
+		if (spinner.counter % spinner.interval == 0)
+		{
+			matrixDrawGlyph(context, glyphs, spinner.glyph_index, spinner.x_index, spinner.y_index, spinner.alpha, "");
+			spinner.y_index++;
+			spinner.glyph_index = randomi(0, 102 - 1);
+			if (spinner.y_index > maximum_glyphs_y)
+			{
+				should_delete = true;
+			}
+		}
+		if (Math.random() < 0.05 / 60.0)
+			should_delete = true;
+		if (spinner.interval > 1)
+		{
+			var percentage = ((spinner.counter % spinner.interval) + 1) / (spinner.interval);
+			matrixDrawGlyph(context, glyphs, spinner.glyph_index, spinner.x_index, spinner.y_index, spinner.alpha * percentage, "");
+		}
+		if (!should_delete)
+			spinners_next.push(spinner);
+		
+		spinner.counter++;
+	}
+	__matrix_spinners = spinners_next;
+	__matrix_spinner_counter++;
+	
+	setTimeout(function() {matrixTick(id)}, __matrix_animation_interval);
+}
+
+function matrixStartAnimation()
+{
+    if (__matrix_animating)
+        return;
+    __matrix_last_event_time = timeInMilliseconds();
+	var canvas_holder = document.getElementById("matrix_canvas_holder");
+    
+    if (canvas_holder === null)
+        return;
+    
+    __matrix_timer_id++;
+	setTimeout(function() {matrixTick(__matrix_timer_id)}, __matrix_animation_interval);
+    __matrix_animating = true;
+    
+    canvas_holder.style.display = "inline";
+    canvas_holder.style.visibility = "visible";
+    canvas_holder.style.opacity = 0.0;
+    canvas_holder.style.transition = "opacity 1.0s";
+    canvas_holder.style.opacity = 1.0;
+}
+
+function matrixStopAnimation()
+{
+    if (!__matrix_animating)
+        return;
+    __matrix_last_event_time = timeInMilliseconds();
+    __matrix_timer_id++; //will cancel any running timer
+    __matrix_animating = false;
+    
+	var canvas_holder = document.getElementById("matrix_canvas_holder");
+    if (canvas_holder == null)
+        return;
+    canvas_holder.style.visibility = "hidden";
+    
+    canvas_holder.style.opacity = 0.0;
+}
+
+
+
+
+
+
+
+//Guide:
+
 //Auto-reload code:
 var __guide_last_reload_api_response; //saved API response from the last time we reloaded
 var __guide_ash_url;
@@ -441,6 +678,7 @@ function updatePageHTML(body_response_string)
 	if (body_response_string.length < 11)
 		return;
 	//Somewhat hacky way of reloading the page:
+    matrixStopAnimation();
     
     //Save display style for two tags:
     //r_location_popup_blackout r_location_popup_box
@@ -528,6 +766,12 @@ function recalculateTimerInterval()
     	__guide_timer_interval = 2500;
     else //huddled in the corner afraid of ascension
     	__guide_timer_interval = 4000;
+    
+    if (__matrix_last_event_time == undefined)
+    	__matrix_last_event_time = timeInMilliseconds();
+    var seconds_since_last_matrix_event = (timeInMilliseconds() - __matrix_last_event_time) / 1000.0;
+    if (Math.min(seconds_since_last_reload, seconds_since_last_matrix_event) > 5 * 60 && !__matrix_animating)
+        matrixStartAnimation();
 }
 
 function updatePageAndFireTimer()
